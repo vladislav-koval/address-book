da7a04a94b6756b8ae0ac1e247cf4479c429bf1e
vladislav-koval <matsu.touta1981@gmail.com>
2019-11-17 01:29:27 +0700

    initial

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, {Fragment} from 'react';
+import ReactDom from 'react-dom';
+import "./style.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => (
+    <Fragment>
+        <Header/>
+        <Main/>
+        <Footer/>
+    </Fragment>
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Header = () => (
+    <div className="header">Header</div>
+);
+
+const Main = () => (
+    <div className="main">
+        <Sidebar/>
+        <Content/>
+    </div>
+);
+
+const Sidebar = () => (
+    <div className="sidebar">Sidebar</div>
+);
+
+const Content = () => (
+    <div className="content">Content</div>
+);
+
+const Footer = () => (
+    <div className="footer">Footer</div>
+);
+
+ReactDom.render(<App/>, document.getElementById('root'));
